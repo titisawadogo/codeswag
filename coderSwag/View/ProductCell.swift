b15e50dca94f0a86408d261ae11db1c186088201
Sawadogo Thierry <dereksawadogo@gmail.com>
2018-05-07 01:20:52 -0400

    updates

---
+++ coderSwag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coderSwag
+//
+//  Created by Sawadogo Thierry on 5/7/18.
+//  Copyright © 2018 Sawadogo Thierry. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
